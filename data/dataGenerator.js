1b9b3e13c9c47b562f1777bb49a6de4bfb079a95
MartinVogelSRH <martin.vogel@stud.hochschule-heidelberg.de>
2018-01-29 19:23:59 +0100

    .

--- data/dataGenerator.js
+++ data/dataGenerator.js
@@ -30,9 +30,6 @@ var insertDocuments = function (db, callback) {
 	collection.insert([
 	  { a: 1 }, { a: 2 }, { a: 3 }
 	], function (err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
 		console.log("Inserted 3 documents into the collection");
 		callback(result);
 	});
