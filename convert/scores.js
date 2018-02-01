cfac33f9b5a4285ae1ce2d5b7c96bfccfd63da0c
Philipp Beck <dertieran@gmail.com>
2018-02-01 17:06:29 +0100

    Games and Coaches

--- convert/scores.js
+++ convert/scores.js
@@ -14,6 +14,8 @@ const auto = require('p-auto');
 const moment = require('moment');
 const config = require('config');
 
+const ora = require('ora');
+
 const MappingService = require('./mappings');
 
 const dumpDB = monk(config.get('databases.fantasy'));
@@ -42,130 +44,129 @@ const buildCoach = fantasy => {
     return { _id, type, firstName, lastName, nationality };
 };
 
-const promisedCoaches = box => {
-    const coaches = [];
-    const mappings = [];
-
-    const awayTeamCoachId = _.get(box, ['AwayTeamCoach', 'CoachId']);
-    if (awayTeamCoachId) {
-        const awayTeamCoach = buildCoach(box.AwayTeamCoach);
-        if (awayTeamCoach) {
-            coaches.push(awayTeamCoach);
-            mappings.push(
-                MappingService.build(
-                    'coach',
-                    awayTeamCoachId,
-                    awayTeamCoach._id
-                )
-            );
-        }
-    } else {
-        console.log(`No AwayTeamCoach in {'Game.GameId': ${box.Game.GameId}}`);
-    }
-
-    const homeTeamCoachId = _.get(box, ['HomeTeamCoach', 'CoachId']);
-    if (homeTeamCoachId) {
-        const homeTeamCoach = buildCoach(box.HomeTeamCoach);
-        if (homeTeamCoach) {
-            coaches.push(homeTeamCoach);
-            mappings.push(
-                MappingService.build(
-                    'coach',
-                    homeTeamCoachId,
-                    homeTeamCoach._id
-                )
-            );
-        }
-    } else {
-        console.log(`No HomeTeamCoach in {'Game.GameId': ${box.Game.GameId}}`);
-    }
-
-    if (_.isEmpty(coaches)) {
-        return Promise.resolve();
-    }
-
-    return Promise.all([
-        footballDB.get('people').insert(coaches),
-        MappingService.insert(mappings),
-    ]);
-};
-
-const promisedGame = (box, mappings, competitions) => {
-    const { Game } = box;
-    if (_.isEmpty(Game)) {
-        return Promise.resolve();
-    }
-
-    const competitionId = competitions[Game.RoundId];
-    const awayTeamId = _.get(mappings, ['teams', Game.AwayTeamId, 'objectId']);
-    const homeTeamId = _.get(mappings, ['teams', Game.HomeTeamId, 'objectId']);
-    if (!competitionId || !awayTeamId || !homeTeamId) {
-        return Promise.resolve();
-    }
-
-    const _id = monk.id();
-    const type = 'coach';
-
-    const visitors = Game.Attendance;
-    const startDate = moment.utc(_.replace(Game.DateTime, /Z?$/, 'Z')).toDate();
-
-    const game = footballDB.get('people').insert({
-        _id,
-        competitionId,
-        awayTeamId,
-        homeTeamId,
-        startDate,
-        visitors,
-    });
-
-    const mapping = MappingService.insert(
-        MappingService.build('game', Game.GameId, _id)
-    );
-
-    return Promise.all([game, mapping]);
-};
-
-const tasks = {
-    rounds: () => dumpDB.get('boxScores').find({}),
-    mappings: () => MappingService.get(['team', 'player', 'season']),
-    competitions: [
-        'rounds',
-        'mappings',
-        ({ rounds, mappings }) =>
+const getMap = (type, ids) =>
+    dumpDB
+        .get('mappings')
+        .find({ type, fantasyId: { $in: ids } })
+        .then(m =>
             _.reduce(
-                rounds,
-                (o, r) => {
-                    o[r.RoundId] = _.get(mappings, [
-                        'season',
-                        r.SeasonId,
-                        'objectId',
-                    ]);
+                m,
+                (o, t) => {
+                    o[t.fantasyId] = t.objectId;
                     return o;
                 },
                 {}
-            ),
-    ],
-    convert: [
-        'mappings',
-        'competitions',
-        ({ mappings, competitions }) =>
+            )
+        );
+
+const run = box =>
+    auto({
+        teams: () => getMap('team', [box.Game.AwayTeamId, box.Game.HomeTeamId]),
+        // players: () => getMap('player', _.map(box.Lineups, 'PlayerId')),
+        competition: () =>
             dumpDB
-                .get('boxScores')
-                .find({})
-                .each((box, { close, pause, resume }) => {
-                    pause();
+                .get('rounds')
+                .findOne({ RoundId: box.Game.RoundId })
+                .then(round =>
+                    dumpDB
+                        .get('mappings')
+                        .findOne({ type: 'season', fantasyId: round.SeasonId })
+                        .then(mapping => {
+                            const r = {};
+                            r[box.Game.RoundId] = mapping.objectId;
+                            return r;
+                        })
+                ),
+        coaches: () => {
+            const coaches = [];
+            const mappings = [];
 
-                    const promises = [];
+            const awayTeamCoachId = _.get(box, ['AwayTeamCoach', 'CoachId']);
+            if (awayTeamCoachId) {
+                const awayTeamCoach = buildCoach(box.AwayTeamCoach);
+                if (awayTeamCoach) {
+                    coaches.push(awayTeamCoach);
+                    mappings.push(
+                        MappingService.build(
+                            'coach',
+                            awayTeamCoachId,
+                            awayTeamCoach._id
+                        )
+                    );
+                }
+            }
 
-                    const coaches = promisedCoaches(box);
-                    const game = promisedGame(box, mappings, competitions);
+            const homeTeamCoachId = _.get(box, ['HomeTeamCoach', 'CoachId']);
+            if (homeTeamCoachId) {
+                const homeTeamCoach = buildCoach(box.HomeTeamCoach);
+                if (homeTeamCoach) {
+                    coaches.push(homeTeamCoach);
+                    mappings.push(
+                        MappingService.build(
+                            'coach',
+                            homeTeamCoachId,
+                            homeTeamCoach._id
+                        )
+                    );
+                }
+            }
 
-                    return Promise.all([coaches, game]).then(() => resume());
-                }),
-    ],
-};
+            if (_.isEmpty(coaches)) {
+                return Promise.resolve();
+            }
 
-auto(tasks)
+            return Promise.all([
+                footballDB.get('people').insert(coaches),
+                MappingService.insert(mappings),
+            ]);
+        },
+        game: [
+            'teams',
+            'competition',
+            ({ teams, competition }) => {
+                const competitionId = competition[box.Game.RoundId];
+                const awayTeamId = teams[box.Game.AwayTeamId];
+                const homeTeamId = teams[box.Game.AwayTeamId];
+                if (!competitionId || !awayTeamId || !homeTeamId) {
+                    return Promise.resolve();
+                }
+
+                const _id = monk.id();
+
+                // const visitors = Game.Attendance;
+                const startDate = moment
+                    .utc(_.replace(box.Game.DateTime, /Z?$/, 'Z'))
+                    .toDate();
+
+                const game = footballDB.get('games').insert({
+                    _id,
+                    competitionId,
+                    awayTeamId,
+                    homeTeamId,
+                    startDate,
+                    // visitors,
+                });
+
+                const mapping = MappingService.insert(
+                    MappingService.build('game', box.Game.GameId, _id)
+                );
+
+                return Promise.all([game, mapping]);
+            },
+        ],
+    });
+
+let counter = 0;
+const spinner = ora().start('Starting ...');
+dumpDB
+    .get('boxScores')
+    .find({})
+    .each((box, { close, pause, resume }) => {
+        pause();
+        counter += 1;
+        spinner.text = `#${counter}/12077`;
+        return run(box).then(() => resume());
+    })
     .then(console.log)
     .catch(console.error)
     .then(exit, exit);
